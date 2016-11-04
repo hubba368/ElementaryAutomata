b82d2b9a326d335b01f64e140084afda195d3fa1
Elliott Bowman <elliott_bowman@hotmail.com>
2016-11-04 22:15:09 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace CellularAutomataGenerator2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(382, 80);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 439);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate - Black and White";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(54, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save File Path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(642, 456);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 29);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reset Image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(461, 439);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 63);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Generate - Random Colours";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(82, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 148);
+            this.panel1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Custom File Path...";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(63, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(108, 68);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(63, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(195, 68);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(63, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Red";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Green";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(214, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Blue";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(91, 116);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 34);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Save Colour Values";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(40, 269);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(279, 189);
+            this.panel2.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(89, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Custom Colour Values...";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(545, 439);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(78, 63);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Generate - User Inputted Values";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(515, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(93, 62);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Help";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 514);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Elementary Cellular Automation";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+//Based off of this person's(Alex Bennett) code here http://timetocode.tumblr.com/post/27473374451/c-cellular-automata-for-procedural-generation
+
+
+
+namespace CellularAutomataGenerator2
+{
+    public partial class Form1 : Form
+    {
+        
+        //MAKE IT SO USER CAN RESET DURING RUNTIME - DONE
+        //IMPROVE RANDOM COLOR GEN - MAKE IT SO USER CAN INPUT COLOUR VALUES - DONE
+
+
+
+
+        //Size of the array.
+        public static int sq = 150;
+        int[,] cells = new int[sq, sq];
+
+        //Dictionary to store the ruleSet.
+        //Tuple is used here instead of say a List as we only need 4 ints (cells + neighbors and result cell)
+        Dictionary<Tuple<int, int, int>, int> ruleSet = new Dictionary<Tuple<int, int, int>, int>();
+
+        string initialFilePath = @"C:\temp\cellular-automata.png";
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+
+        //Create the base map which is an array of 0s.
+        public void CreateBaseMap()
+        {
+            for (int i = 0; i < sq; i++)
+            {
+                for (int j = 0; j < sq; j++)
+                {
+                    cells[i, j] = 0;
+                }                    
+            }                
+        }
+        
+        //Create the starting point of the automation by putting 1s or 0s in pseudo random places on the array.
+        public void CreateInitialMap()
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < sq; i++)
+            {
+                cells[i, 0] = r.Next(0, 2); 
+            }
+                
+        }
+        
+
+        //This is the elementary cellular automata ruleset, which makes use of an 8 binary number to generate the cells.
+        public void AddRules()
+        {
+            ruleSet.Add(new Tuple<int, int, int>(0, 0, 0), 1);
+            ruleSet.Add(new Tuple<int, int, int>(0, 0, 1), 0);
+            ruleSet.Add(new Tuple<int, int, int>(0, 1, 0), 0);
+            ruleSet.Add(new Tuple<int, int, int>(0, 1, 1), 1);
+            ruleSet.Add(new Tuple<int, int, int>(1, 0, 0), 1);
+            ruleSet.Add(new Tuple<int, int, int>(1, 0, 1), 1);
+            ruleSet.Add(new Tuple<int, int, int>(1, 1, 0), 1);
+            ruleSet.Add(new Tuple<int, int, int>(1, 1, 1), 0);
+        }
+        
+
+        //generates the cellular automation by going through each cell on the array.
+        public void GenerateCells()
+        {
+            //For each cell in the array... 
+            for (int i = 1; i < sq; i++)
+            {
+                for (int j = 0; j < sq; j++)
+                {
+                    //Find the left(j - 1), middle(i - 1) and right(j + 1) cells for each cell.
+                    int left = (j == 0) ? left = cells[sq - 1, i - 1] : left = cells[j - 1, i - 1];
+
+                    int middle = cells[j, i - 1];
+
+                    int right = (j == sq - 1) ? right = cells[0, i - 1] : right = cells[j + 1, i - 1];
+
+                    int result = ruleSet[new Tuple<int, int, int>(left, middle, right)];
+
+                    cells[j, i] = result;
+                }
+            }
+        }
+
+
+        //Saves an image of the result to a .png file.
+        public void SaveImage()
+        {
+            Bitmap img = new Bitmap(sq, sq);
+            for (int i = 0; i < sq; i++)
+            {
+                for (int j = 0; j < sq; j++)
+                {
+                    Color g = (cells[i, j] == 1) ? Color.Black : Color.White;
+                    img.SetPixel(i, j, g);
+                }
+            }
+            MessageBox.Show("Save Complete.");
+            img.Save(initialFilePath);
+        }
+
+
+        //Shows the image on the picturebox.
+        public void ShowImage()
+        {
+            Image image = Image.FromFile(initialFilePath);
+            if (pictureBox2.Image != null)
+            {
+                MessageBox.Show("Error: Please delete the current image first.");
+            }
+            else
+            {               
+                if(image.Width < pictureBox2.Width && image.Height < pictureBox2.Height)
+                {
+                    pictureBox2.Image = image;
+                    pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+                else
+                {
+                    pictureBox2.Image = image;
+                    pictureBox2.Width = image.Width;
+                    pictureBox2.Height = image.Height;
+                }
+
+            }           
+        }
+
+
+        //Resets the picturebox and the Dictionary.
+        public void ResetImage()
+        {
+            pictureBox2.Image = null;
+            ruleSet.Clear();
+            
+            MessageBox.Show("Image Reset.");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //Change file path
+        private void button2_Click(object sender, EventArgs e)
+        {
+            initialFilePath = textBox1.Text;
+        }
+
+
+        //Generate during runtime - Black and White colours.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateBaseMap();
+            AddRules();
+            CreateInitialMap();
+            GenerateCells();
+            SaveImage();
+            ShowImage();
+            button1.Enabled = false;
+            button4.Enabled = false;
+            button6.Enabled = false;
+        }
+
+
+        //Reset the picturebox.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ResetImage();
+            button1.Enabled = true;
+            button4.Enabled = true;
+            button6.Enabled = true;
+        }
+
+        //Save Image with Psuedo Random pixel colours.
+        public void SaveImageRandom()
+        {
+            Bitmap img = new Bitmap(sq, sq);
+            Random rand = new Random();
+            for (int i = 0; i < sq; i++)
+            {
+                for (int j = 0; j < sq; j++)
+                {
+                    Color test = (cells[i, j] == 1) ? Color.FromArgb(rand.Next(70,200),rand.Next(70,200),rand.Next(70,100)) : Color.White;
+                    img.SetPixel(i, j, test);
+                }
+            }
+            MessageBox.Show("Save Complete.");
+            img.Save(initialFilePath);
+        }
+
+
+        //Save image with User inputted colour values.
+        public void SaveImageUserInput()
+        {
+            Bitmap img = new Bitmap(sq, sq);
+            Random rand = new Random();
+            for (int i = 0; i < sq; i++)
+            {
+                for (int j = 0; j < sq; j++)
+                {
+                    Color test = (cells[i, j] == 1) ? Color.FromArgb(red, green, blue) : Color.White;
+                    img.SetPixel(i, j, test);
+                }
+            }
+            MessageBox.Show("Save Complete.");
+            img.Save(initialFilePath);
+        }
+
+
+        //generate an image with pseudo random colours.
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CreateBaseMap();
+            AddRules();
+            CreateInitialMap();
+            GenerateCells();
+            SaveImageRandom();
+            ShowImage();
+            button4.Enabled = false;
+            button1.Enabled = false;
+            button6.Enabled = false;
+        }
+
+
+        //Save user inputted colour values.
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int redValue;
+            int greenValue;
+            int blueValue;
+
+            if(int.TryParse(textBox2.Text, out redValue))
+            {
+                red = redValue;
+                MessageBox.Show("Values Saved.");
+            }
+            else if(redValue > 255)
+            {
+                MessageBox.Show("Invalid Number.");
+            }
+            else
+            {
+                MessageBox.Show("Numbers Only!");
+                return;
+            }
+
+            if (int.TryParse(textBox3.Text, out greenValue))
+            {
+                green = greenValue;
+            }
+            else if (greenValue > 255)
+            {
+                MessageBox.Show("Invalid Number.");
+            }
+            else
+            {
+                MessageBox.Show("Numbers Only!");
+                return;
+            }
+
+            if (int.TryParse(textBox4.Text, out blueValue))
+            {
+                blue = blueValue;
+            }
+            else if (blueValue > 255)
+            {
+                MessageBox.Show("Invalid Number.");
+            }
+            else
+            {
+                MessageBox.Show("Numbers Only!");
+                return;
+            }
+        }
+
+        //Show image with user inputted colour values.
+        private void button6_Click(object sender, EventArgs e)
+        {
+            CreateBaseMap();
+            AddRules();
+            CreateInitialMap();
+            GenerateCells();
+            SaveImageUserInput();
+            ShowImage();
+            button1.Enabled = false;
+            button4.Enabled = false;
+            button6.Enabled = false;
+        }
+
+        //Show help.
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("To change the range for the pseudo-random colour values, change them from within 'SaveImageRandom' method. \n");
+        }
+    }
+}
